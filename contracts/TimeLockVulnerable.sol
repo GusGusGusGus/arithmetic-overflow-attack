e36d589d34b45a9d86556820f61c45d24542565b
QuickTriXXX <isthisreal@whatever.com>
2022-05-30 16:32:47 +0100

    created vunerable timelock deposit contract

---
+++ contracts/TimeLockVulnerable.sol
@@ -0,0 +1,42 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity 0.7.6;
+
+import "hardhat/console.sol";
+
+/// @title This VULNERABLE contract allow a user to deposit funds, thereby locking them for a week.
+/// @author GusGusGusGus
+/// @notice Deposit money to the contract and cry.
+/// @dev Purposefully outdated version of the compiler (0.7.6) and no support for safe arithmetic operations
+contract TimeLockVulnerable {
+    mapping(address => uint) public balances;
+    mapping(address => uint) public locktime;
+
+    /// @notice Function that allows user to deposit their funds to be timelocked
+    /// @dev payable function that saves the sent value on-chain and the locktime value (timestamp of the current block + 1 week) to the locktime array
+    function deposit() external payable {
+        balances[msg.sender] += msg.value;
+        balances[msg.sender] = block.timestamp + 1 weeks;
+    }
+
+    /// @notice Allows a user to increase the amount of time their tokens are locked
+    /// @dev Increases locktime property value to received arguments 
+    /// @param _secondsToIncrease in seconds
+    function increaseTimeLock(uint _secondsToIncrease) public {
+        locktime[msg.sender] += _secondsToIncrease;
+        console.log("Increased timelock '%s' seconds...", _secondsToIncrease);
+    } 
+
+    /// @notice Function that allows user to withdraw their funds
+    /// @dev VULNERABLE withdraw function
+    function withdraw() public { 
+        require(balances[msg.sender] > 0, "Insufficient funds");
+        //vulnerable
+        require(block.timestamp > locktime[msg.sender], "Lock time has not expired yet");
+
+        uint amount = balances[msg.sender];
+        balances[msg.sender] = 0;
+
+        (bool sent, ) = msg.sender.call{value: amount}("");
+        require(sent, "Failed to send Ether");
+    }
+}
